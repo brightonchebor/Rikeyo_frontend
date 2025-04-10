9b24973c6bce8f78a04bb44555220588b43194b9
brightonchebor <cheborbrighton805@gmail.com>
2025-04-10 12:07:36 +0300

    cn

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Button from './components/common/Button'
+import Card from './components/common/Card'
 
 function App() {
   
@@ -8,6 +9,8 @@ function App() {
   return (
     <>
       <div>
+        <Button />
+        <Card />
         <Button />
       </div>
      
